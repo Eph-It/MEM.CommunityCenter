8557c78ac7445afe31e784f4b077d2ceb56f86a1
Ryan Ephgrave <Ryan2065@gmail.com>
2020-04-26 10:13:58 -0500

    updated

--- CommunityCenter/CommunityCenter.WebAPI/Server/Startup.cs
+++ CommunityCenter/CommunityCenter.WebAPI/Server/Startup.cs
@@ -1,6 +1,10 @@
+using CommunityCenter.CM.DB;
+using CommunityCenter.User;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
@@ -9,11 +13,27 @@ namespace CommunityCenter.WebAPI.Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+        public IConfiguration Configuration { get; }
+        IWebHostEnvironment _env;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            if (_env.IsDevelopment())
+            {
+                services.AddScoped<ICCUser, CMTestUser>();
+            }
+            else
+            {
+                services.AddScoped<ICCUser, IISUser>();
+            }
+            services.AddDbContext<CMDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MEMCMDB")));
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
@@ -22,11 +42,11 @@ namespace CommunityCenter.WebAPI.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseResponseCompression();
 
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBlazorDebugging();
