0e77a132b8d2e72954e360a2b73e8c3d32abdff4
Ryan Ephgrave <ryan2065@gmail.com>
2020-05-03 17:41:12 -0500

    added getting column info

--- CommunityCenter/CommunityCenter.CM.DB/CMDB.cs
+++ CommunityCenter/CommunityCenter.CM.DB/CMDB.cs
@@ -24,10 +24,17 @@ namespace CommunityCenter.CM.DB
         private string _cmUserSIDs;
         private void RefreshCMUserSIDs()
         {
-            string query = "select dbo.fn_rbac_GetAdminIDsfromUserSIDs(@userSids) as Result";
-            Dictionary<string, object> Params = new Dictionary<string, object>();
-            Params.Add("userSids", _user.GetSids());
-            _cmUserSIDs = _ccSQL.ExecuteQuery(query, Params).Results[0][0].ToString();
+            if(_cache.TryGetValue(_user.GetSids(), out _cmUserSIDs))
+            {
+                string query = "select dbo.fn_rbac_GetAdminIDsfromUserSIDs(@userSids) as Result";
+                Dictionary<string, object> Params = new Dictionary<string, object>();
+                Params.Add("userSids", _user.GetSids());
+                _cmUserSIDs = _ccSQL.ExecuteQuery(query, Params).Results[0][0].ToString();
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+                _cache.Set(_user.GetSids(), _cmUserSIDs, cacheEntryOptions);
+            }
+            
         }
         public string CMUserSIDs
         {
@@ -41,5 +48,36 @@ namespace CommunityCenter.CM.DB
             }
         }
         
+        public List<CMDBColumn> GetTableColumns(CCObject ccObject)
+        {
+            List<CMDBColumn> columns = new List<CMDBColumn>();
+            if (_cache.TryGetValue($"GetTableColumns.{ccObject.Table}", out columns))
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(10));
+
+                string query = @"
+                    SELECT col.name AS 'ColumnName'
+		                    ,typ.name as 'ColumnType'
+                        from sys.columns col
+                            join sys.types typ on
+                                col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
+                        where object_id = object_id(@tableName)
+                ";
+                Dictionary<string, object> Params = new Dictionary<string, object>();
+                Params.Add("tableName", ccObject.Table);
+                var results = _ccSQL.ExecuteQuery(query, Params).Results;
+                foreach(var result in results)
+                {
+                    var cmDBColumn = new CMDBColumn();
+                    cmDBColumn.ColumnName = result[0].ToString();
+                    cmDBColumn.ColumnType = result[1].ToString();
+                    columns.Add(cmDBColumn);
+                }
+
+                _cache.Set($"GetTableColumns.{ccObject.Table}", columns, cacheEntryOptions);
+            }
+            return columns;
+        }
     }
 }
---
+++ CommunityCenter/CommunityCenter.CM.DB/CMDBColumn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityCenter.CM.DB
+{
+    public class CMDBColumn
+    {
+        public string ColumnName { get; set; }
+        public string ColumnType { get; set; }
+    }
+}
